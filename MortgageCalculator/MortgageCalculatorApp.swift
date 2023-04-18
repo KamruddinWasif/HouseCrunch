bbbc450188e160c8618478a5507f69b9e58d9406
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-04-18 23:41:52 +1000

    Initial Commit

---
+++ MortgageCalculator/MortgageCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortgageCalculatorApp.swift
+//  MortgageCalculator
+//
+//  Created by Wasif Kamruddin on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MortgageCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
